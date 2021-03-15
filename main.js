2dadbef2a7a9c7176b6c0ab08e90803a55f0d93a
ice2me <ice2me1989@gmail.com>
2021-03-15 18:09:52 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+import Game from './src/game.js'
+import View from './src/view.js'
+
+const root = document.querySelector('#root')
+
+const game = new Game()
+const view = new View(root, 320, 640, 20, 10)
+
+window.game = game
+window.view = view
+
+
+document.addEventListener('keydown', (e) => {
+	switch (e.keyCode) {
+		case 37:
+			game.movePieceLeft()
+			view.render(game.getState())
+			break
+		case 38:
+			game.rotatePiece()
+			view.render(game.getState())
+			break
+		case 39:
+			game.movePieceRight()
+			view.render(game.getState())
+			break
+		case 40:
+			game.movePieceDown()
+			view.render(game.getState())
+			break
+	}
+})
+
+
